aae3f8e297c42955b271a7c5258cc6be6c94222f
noraaldossary <nnxh.1@hotmail.com>
2023-11-28 09:30:50 +0300

    Initial Commit

---
+++ VideoPlayScreen/VideoPlayScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayScreenApp.swift
+//  VideoPlayScreen
+//
+//  Created by Nora Aldossary on 14/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
